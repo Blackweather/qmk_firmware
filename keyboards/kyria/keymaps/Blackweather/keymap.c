870dbb0b72bd0f121c78064d1520ec607e6ee658
Karol Szczepański <szczepanski.karol0@gmail.com>
2021-01-11 20:15:46 +0100

    fix: altgr modtap

--- keyboards/kyria/keymaps/Blackweather/keymap.c
+++ keyboards/kyria/keymaps/Blackweather/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_DHM] = LAYOUT(
       KC_NO, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_QUOT, KC_NO,
       KC_NO, MT(MOD_LGUI,KC_A), MT(MOD_LALT, KC_R), MT(MOD_LCTL, KC_S), MT(MOD_LSFT, KC_T), KC_G, KC_M, MT(MOD_RSFT, KC_N), MT(MOD_RCTL, KC_E), MT(MOD_LALT, KC_I), MT(MOD_RGUI, KC_O), KC_NO,
-      KC_LSFT, KC_Z, MT(KC_ALGR, KC_X), KC_C, KC_D, KC_V, KC_LSFT, KC_LSFT, KC_NO, KC_LSFT, KC_K, KC_H, KC_COMM, MT(KC_ALGR, KC_DOT), KC_SLSH, KC_NO,
+      KC_LSFT, KC_Z, MT(MOD_RALT, KC_X), KC_C, KC_D, KC_V, KC_LSFT, KC_LSFT, KC_NO, KC_LSFT, KC_K, KC_H, KC_COMM, MT(MOD_RALT, KC_DOT), KC_SLSH, KC_NO,
       KC_LGUI, LT(_MEDIA, KC_ESC), LT(_NAV, KC_SPC), LT(_MOUSE, KC_TAB), KC_NO, KC_NO, LT(_SYM, KC_ENT), LT(_NUM, KC_BSPC), LT(_FUNC, KC_DEL), KC_MUTE
     ),
 
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //   KC_ESC,    KC_Q, KC_W, KC_E, KC_R, KC_T,                                     KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_PIPE,
     //   MT(MOD_LCTL, KC_BSPC), KC_A, KC_S, KC_D, KC_F, KC_G,                                     KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
     //   KC_LSFT,               KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LSFT, KC_LSFT, KC_TAB, KC_LSFT,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
-    //                         KC_LGUI, KC_DEL, MT(MOD_LALT, KC_SPC), LT(_LOWER, KC_ENT), LT(_RAISE, KC_ESC), LT(_LOWER, KC_ENT), LT(_RAISE, KC_SPC), KC_ALGR, KC_BSPC, KC_MUTE
+    //                         KC_LGUI, KC_DEL, MT(MOD_LALT, KC_SPC), LT(_LOWER, KC_ENT), LT(_RAISE, KC_ESC), LT(_LOWER, KC_ENT), LT(_RAISE, KC_SPC), MOD_RALT, KC_BSPC, KC_MUTE
     // ),
 
 /*
